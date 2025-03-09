f4e7e9ea4e5e6a48b63511ae083b9cafef21beda
Erdem Kilci <erdem.kilci024@gmail.com>
2025-03-09 15:53:27 +0100

    making it static

--- obj/Debug/net9.0/CarRentalUI.AssemblyInfo.cs
+++ obj/Debug/net9.0/CarRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354f2e506e17e1c9af7dac7bf026062adbddd838")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
